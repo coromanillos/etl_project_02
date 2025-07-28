5409698709359d8947a56a4419f8ca41eb41b795
Christopher <cromanillos99@cromDebDesktop.myfiosgateway.com>
2025-07-27 23:41:05 -0400

    Searching for API

---
+++ src/extract.py
@@ -0,0 +1,6 @@
+###########################################
+# Name: extract.py
+# Author: Christopher O. Romanillos
+# Description: REST API Extraction script
+# Data: 07/27/25
+###########################################
